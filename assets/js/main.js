6aa51e0ec708a87e593cd01241ba7214b633dc9f
softcommteam2 <softcommteam2@gmail.com>
2022-11-19 15:51:15 +0630

    yms

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+new Swiper(".clientSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true
+  });
