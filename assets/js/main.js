dd15b93208719766ee9ad2ade2c56aae58583387
softcommteam2 <softcommteam2@gmail.com>
2022-11-21 16:54:48 +0630

    yms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+// close-menu-btn
+// open-menu-btn
+// mobile-nav
+
+const mobileNav = document.querySelector('#mobile-nav')
+const closeMenuBtn = document.querySelector('#close-menu-btn')
+const openMenuBtn = document.querySelector('#open-menu-btn')
+
+openMenuBtn.onclick = ()=> {
+  mobileNav.style.display = 'block'
+}
+closeMenuBtn.onclick = ()=> {
+  mobileNav.style.display = 'none'
+}
+
+// Client section 
 new Swiper(".clientSwiper", {
     pagination: {
       el: ".swiper-pagination",
@@ -9,3 +25,6 @@ new Swiper(".clientSwiper", {
     },
     loop: true
   });
+
+  // Aos 
+  AOS.init();
